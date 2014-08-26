d69a51c003a9db7f8646d3065ef24fa77d77e860
sdierauf <sdierauf@gmail.com>
2014-08-26 00:05:58 -0700

    added bunnies

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+
+var bunnies = {
+    greeting: "Welcome to Stefan's officially unofficial api, bunnies!",
+    available: [
+        site_url: "http://stfn.me"
+    ]
+}
+
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+    res.json(bunnies);
 });
 
 module.exports = router;
